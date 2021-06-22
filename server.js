55ececb18a17f7f21724fdfdf8f74f8f0d0b6699
JiaWangGan <kingwanggan@gmail.com>
2021-06-22 23:06:48 +0800

    test-deploy

---
+++ server.js
@@ -0,0 +1,17 @@
+// const PORT = process.env.PORT || 5000;
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const server = http.Server(app);
+
+// app.use(express.static("public"));
+
+// app.listen(PORT, () => console.log("Listening on Port", PORT));
+
+// const io = require("socket.io")(server);
+
+// io.on("connection", (socket) => {
+//   socket.on("message", (msg) => {
+//     io.emit("message", msg);
+//   });
+// });
