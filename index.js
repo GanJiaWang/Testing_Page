3346d6d5fe4eacf2fafaffb7753dce99225c2930
JiaWangGan <kingwanggan@gmail.com>
2021-06-22 22:25:51 +0800

    Add procfile

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 const http = require("http");
-const server = http.Serve(app);
+const server = http.Server(app);
 
 app.use(express.static("public"));
 
